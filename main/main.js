0dc61c6550794d8e09143356934e17fffd70bca9
No17fox <buptwanglei@163.com>
2018-09-25 20:44:05 +0800

    write my code

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(Num) {
   // Write your code here
+  var len = Num.length;
+  var sum = 0;
+  for (var i=0; i<len; i++){
+      sum += Number(Num[i]);
+  }
+  return sum;
 };
+
